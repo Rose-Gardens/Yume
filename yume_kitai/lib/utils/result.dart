f491b04dc26b75bef2589b143ee20618034a8d03
GetPsyched <roshin006@gmail.com>
2025-03-12 17:59:30 +0400

    comment fix

--- yume_kitai/lib/utils/result.dart
+++ yume_kitai/lib/utils/result.dart
@@ -1,6 +1,5 @@
 // Copyright 2024 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Use of this source code is governed by a BSD-style license.
 
 /// Utility class to wrap result data
 ///
