0c06d68d21045f89d49815bfc6c975c307364fa8
GetPsyched <roshin006@gmail.com>
2025-03-24 06:22:48 +0400

    Added animations and content to the popup menu

--- yume_kitai/lib/ui/core/ui/popup_menu.dart
+++ yume_kitai/lib/ui/core/ui/popup_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:yume_kitai/ui/core/ui/blur_saturate_filter.dart';
 
 import '../themes/theme_extension.dart';
@@ -13,30 +14,35 @@ class PopupMenu extends StatefulWidget {
 class PopupMenuState extends State<PopupMenu>
     with SingleTickerProviderStateMixin {
   OverlayEntry? _overlayEntry;
-  late AnimationController _animationController;
+  late final AnimationController _animationController;
 
   @override
   void initState() {
     _animationController = AnimationController(
-      duration: const Duration(milliseconds: 200),
+      reverseDuration: const Duration(milliseconds: 150),
       vsync: this,
     );
     super.initState();
   }
 
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void showOverlay(Offset globalPos) {
-    if (_overlayEntry != null) {
-      _overlayEntry!.remove();
-      _overlayEntry = null;
-    }
+    _hideOverlay(); // ? Avoids multiple overlays (I assume)
 
     // > Part of showOverlay
     _overlayEntry = OverlayEntry(
       builder: (context) {
         final theme = Theme.of(context).extension<AppThemeExtension>()!;
+        final screenSize = MediaQuery.of(context).size;
+
         return SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
+          width: screenSize.width,
+          height: screenSize.height,
           child: Stack(
             children: [
               ModalBarrier(
@@ -45,21 +51,81 @@ class PopupMenuState extends State<PopupMenu>
                 },
               ),
               Positioned(
-                top: globalPos.dy,
-                left: globalPos.dx,
+                top: globalPos.dy - 50,
+                left: globalPos.dx - 50,
                 child: ClipRRect(
-                  borderRadius: BorderRadius.circular(24.0),
+                  borderRadius: BorderRadius.circular(16.0),
                   child: BackdropFilter(
-                    filter: blurSaturateFilter,
+                    filter: blurSaturateFilterPerformance,
                     child: Container(
-                      width: 200,
-                      height: 100,
+                      width: 0.4 * screenSize.width,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
                       decoration: BoxDecoration(
-                          color: theme.surfaceOverlay.withValues(alpha: 0.25)),
-                      child: const Text("HIIIIIIIIIIIIIIIII"),
+                          color: theme.surfaceOverlay.withValues(alpha: 0.6)),
+                      child: const Column(
+                        spacing: 16,
+                        children: [
+                          Row(
+                            children: [
+                              Text("Complete"),
+                              Spacer(),
+                              Icon(Icons.check_circle_outline_outlined)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Skip"),
+                              Spacer(),
+                              Icon(Icons.skip_next_outlined)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Vacation"),
+                              Spacer(),
+                              Icon(Icons.houseboat_outlined)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Set Tag"),
+                              Spacer(),
+                              Icon(Icons.sell_outlined)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Retire"),
+                              Spacer(),
+                              Icon(Icons.exit_to_app_rounded)
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Text("Delete"),
+                              Spacer(),
+                              Icon(Icons.delete_outline_rounded)
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
+                )
+                    .animate(
+                      controller: _animationController,
+                    )
+                    .fadeIn(
+                      duration: 300.ms,
+                      curve: Curves.easeOutQuint,
+                      begin: 0,
+                    )
+                    .scale(
+                      duration: 300.ms,
+                      curve: Curves.easeOutBack,
+                      begin: const Offset(0.6, 0.6),
+                    ),
               ),
             ],
           ),
@@ -71,9 +137,12 @@ class PopupMenuState extends State<PopupMenu>
 
   void _hideOverlay() {
     if (_overlayEntry != null) {
-      _overlayEntry!.remove();
-      _overlayEntry = null;
-      return;
+      _animationController.reverse().whenCompleteOrCancel(
+        () {
+          _overlayEntry!.remove();
+          _overlayEntry = null;
+        },
+      );
     }
   }
 
