7ee56d4c3e5f51e7e8e229e441117ef45ca8d686
Roshin Nishad <roshin006@gmail.com>
2025-05-06 20:52:41 +0400

    cleaned up habit files

--- yume_kitai/lib/ui/habits/widgets/habits_card.dart
+++ yume_kitai/lib/ui/habits/widgets/habits_card.dart
@@ -3,76 +3,101 @@ import 'package:flutter/material.dart';
 import 'package:smooth_corner/smooth_corner.dart';
 import 'package:yume_kitai/utils/converters.dart';
 
+import '../../../domain/models/habit/habit/habit.dart';
 import '../../core/themes/theme_extension.dart';
-
-import 'habits_shrinkable_card.dart';
 
 class HabitsCard extends StatelessWidget {
   const HabitsCard({
     super.key,
-    required this.widget,
+    required this.habit,
   });
 
-  final HabitsShrinkableCard widget;
+  final Habit habit;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).extension<AppThemeExtension>()!;
 
-    final Color cardColor = widget.habit.color.isEmpty
+    final Color cardColor = habit.color.isEmpty
         ? theme.surfaceMedium
-        : Color(hexStringToHexInt(widget.habit.color)).withValues(alpha: 0.4);
+        : Color(hexStringToHexInt(habit.color)).withValues(alpha: 0.4);
 
-    final Color iconColor = widget.habit.color.isEmpty
-        ? theme.foregroundHigh
-        : Color(hexStringToHexInt(widget.habit.color)).withValues(alpha: 0.85);
+    final Color iconColor = habit.color.isEmpty
+        ? theme.foregroundMedium
+        : Color(hexStringToHexInt(habit.color)).withValues(alpha: 0.85);
 
-    final cardBorderRadius = (widget.habit.title.length > 25)
-        ? 26.0
-        : 18.0; // Bigger vs. smaller card
+    final Widget iconWidget = habit.icon.isNotEmpty
+        ? Icon(
+            IconData(hexStringToHexInt(habit.icon),
+                fontFamily: 'MaterialIcons'),
+            size: 40,
+            color: iconColor)
+        : Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: cardColor,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  color: iconColor,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ],
+          );
 
-    return Card(
-      elevation: 0,
+    return AnimatedContainer(
+      // TODO: Figure out how to do swipe
+      width: MediaQuery.of(context).size.width,
+      duration: const Duration(milliseconds: 500),
+      constraints: BoxConstraints(
+          minHeight: 56,
+          maxHeight: 96,
+          minWidth: 56,
+          maxWidth: MediaQuery.of(context).size.width),
       margin: EdgeInsets.zero,
-      shape: SmoothRectangleBorder(
-        borderRadius: BorderRadius.circular(cardBorderRadius),
-        smoothness: 0.6,
-        side: BorderSide(
-          color: theme.borderLow,
-          width: 0.5,
+      decoration: ShapeDecoration(
+        color: cardColor,
+        shape: SmoothRectangleBorder(
+          borderRadius: BorderRadius.circular(22.0),
+          smoothness: 0.6,
+          side: BorderSide(
+            color: theme.borderLow,
+            width: 0.5,
+          ),
         ),
       ),
-      color: cardColor,
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         child: Row(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            if (widget.habit.icon.isNotEmpty)
-              Icon(
-                  IconData(hexStringToHexInt(widget.habit.icon),
-                      fontFamily: 'MaterialIcons'),
-                  size: 40,
-                  color: iconColor),
+            iconWidget,
             const SizedBox(width: 8),
-            Expanded(
-              child: Padding(
-                // Text has separate padding from card to even unevenness from icon padding
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.habit.title,
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleLarge
-                          ?.copyWith(color: theme.foregroundHigh),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
-                ),
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    habit.title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: theme.foregroundHigh),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
               ),
             ),
           ],
