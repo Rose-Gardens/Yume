a315d1ac24ca447a38fbc658dff2a11f8f471b38
Roshin Nishad <roshin006@gmail.com>
2025-05-16 19:02:45 +0400

    added state to menucontent, initial selection test

--- yume_kitai/lib/ui/core/ui/popup_menu/popup_menu_content.dart
+++ yume_kitai/lib/ui/core/ui/popup_menu/popup_menu_content.dart
@@ -1,61 +1,95 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:yume_kitai/ui/core/ui/blur_saturate_filter.dart';
 import 'package:yume_kitai/ui/core/ui/popup_menu/popup_menu_item_tile.dart';
 
 import '../../themes/theme_extension.dart';
 
-class PopupMenuContent extends StatelessWidget {
+class PopupMenuContent extends StatefulWidget {
   const PopupMenuContent({
     super.key,
-    required this.theme,
     required this.globalPos,
     required this.menuData,
+    required this.menuHeight,
+    required GlobalKey menuKey,
     required AnimationController animationController,
-  }) : _animationController = animationController;
+  })  : _animationController = animationController,
+        _menuKey = menuKey;
 
-  final AppThemeExtension theme;
   final Offset globalPos;
-  final List<({String title, IconData icon, bool isDanger})> menuData;
+  final GlobalKey _menuKey;
+  final double? menuHeight;
   final AnimationController _animationController;
+  final List<({String title, IconData icon, bool isDanger})> menuData;
+
+  @override
+  State<PopupMenuContent> createState() => _PopupMenuContentState();
+}
+
+class _PopupMenuContentState extends State<PopupMenuContent> {
+  static const double menuWidth = 175;
+  double get topPosition => widget.globalPos.dy - 50;
+  double get leftPosition => widget.globalPos.dx - 50;
+  int? selectedIndex;
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).extension<AppThemeExtension>()!;
+
     return Positioned(
-      top: globalPos.dy - 50,
-      left: globalPos.dx - 50,
+      top: topPosition,
+      left: leftPosition,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16.0),
         child: BackdropFilter(
           filter: blurSaturateFilterPerformance,
-          child: Container(
-            width: 175,
-            decoration: BoxDecoration(
-              color: theme.surfaceOverlay.withValues(alpha: 0.8),
-            ),
-            child: ListView.separated(
-              shrinkWrap: true,
-              itemCount: menuData.length,
-              separatorBuilder: (BuildContext context, int index) => Divider(
-                height: 1,
-                thickness: 1,
-                color: theme.surfaceMedium,
+          child: GestureDetector(
+            onPanUpdate: (details) {
+              if (details.localPosition.dx >= 0 &&
+                  details.localPosition.dx <= menuWidth) {
+                if (details.localPosition.dy >= 0 &&
+                    details.localPosition.dy <=
+                        ((widget.menuHeight! - 5) / 6)) {
+                  setState(() {
+                    selectedIndex = 0;
+                  });
+                }
+              }
+            },
+            child: Container(
+              key: widget._menuKey,
+              width: menuWidth,
+              decoration: BoxDecoration(
+                color: theme.surfaceOverlay.withValues(alpha: 0.8),
               ),
-              itemBuilder: (BuildContext context, int index) {
-                final menuItem = menuData[index];
-                final textColor =
-                    menuItem.isDanger ? theme.danger : theme.foregroundBright;
-                return PopupMenuItemTile(
-                  menuItem: menuItem,
-                  textColor: textColor,
-                );
-              },
+              child: ListView.separated(
+                shrinkWrap: true,
+                itemCount: widget.menuData.length,
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  height: 1,
+                  thickness: 1,
+                  color: theme.surfaceMedium,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  final menuItem = widget.menuData[index];
+                  final textColor = menuItem.isDanger
+                      ? theme.danger
+                      : theme.foregroundBright;
+                  return PopupMenuItemTile(
+                    menuItem: menuItem,
+                    textColor: textColor,
+                    currentIndex: index,
+                    selectedIndex: selectedIndex,
+                  );
+                },
+              ),
             ),
           ),
         ),
       )
           .animate(
-            controller: _animationController,
+            controller: widget._animationController,
           )
           .fadeIn(
             duration: 300.ms,
