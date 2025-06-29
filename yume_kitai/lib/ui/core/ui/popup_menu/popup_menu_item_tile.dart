01d875a732d66026e9f9bc7a5786ca8ca4cce3b2
Roshin Nishad <roshin006@gmail.com>
2025-06-29 18:40:49 +0530

    added copyright notice

--- yume_kitai/lib/ui/core/ui/popup_menu/popup_menu_item_tile.dart
+++ yume_kitai/lib/ui/core/ui/popup_menu/popup_menu_item_tile.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 (c) Roshin Nishad. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 License that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_sficon/flutter_sficon.dart';
--- yume_kitai/lib/utils/swipe_direction.dart
+++ yume_kitai/lib/utils/swipe_direction.dart
@@ -1,1 +1,5 @@
+// Copyright 2025 (c) Roshin Nishad. All rights reserved.
+// Use of this source code is governed by the Apache 2.0 License that can be
+// found in the LICENSE file.
+
 enum SwipeDirection { none, up, down, left, right }
