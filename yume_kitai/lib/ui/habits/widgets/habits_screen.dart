fe602f8c851d1c5a2aabcc85bb48c97c9b6c8906
Roshin Nishad <roshin006@gmail.com>
2025-07-12 19:22:56 +0530

    Changed ListView to CustomScrollView for Slivers

--- yume_kitai/lib/ui/habits/widgets/habits_screen.dart
+++ yume_kitai/lib/ui/habits/widgets/habits_screen.dart
@@ -8,6 +8,7 @@ import 'package:yume_kitai/ui/core/ui/popup_menu/popup_menu.dart';
 import 'package:yume_kitai/ui/habits/view_models/habits_viewmodel.dart';
 
 import 'package:yume_kitai/ui/habits/widgets/habits_gesturable_card.dart';
+import 'package:yume_kitai/ui/habits/widgets/habits_sliver_appbar.dart';
 
 class HabitsScreen extends StatefulWidget {
   const HabitsScreen({super.key});
@@ -22,49 +23,52 @@ class _HabitsScreenState extends State<HabitsScreen> {
     final viewModel = context.watch<HabitsViewModel>();
     final overlayKey = context.read<GlobalKey<PopupMenuState>>();
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: ListenableBuilder(
-        // Listen to changes in the loadHabits function
-        listenable: viewModel.loadHabits,
-        builder: (context, child) {
-          // If the function is still running, show a progress indicator.
-          if (viewModel.loadHabits.running || viewModel.habits == null) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          // TODO: make an error widget
-          // If the function failed, press to try again.
-          if (viewModel.loadHabits.error) {
-            return const SizedBox();
-          }
-          // * If not error, or running, filter habits and build list.
-          final filteredHabits = viewModel.habits!
-              .where((habit) => !habit.isRetired)
-              .toList();
-
-          return Stack(
-            children: [
-              ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                padding: const EdgeInsets.only(top: 24),
-                itemCount: filteredHabits.length,
-                itemBuilder: (context, index) {
-                  final habit = filteredHabits[index];
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 12),
-                    child: HabitsGesturableCard(
-                      habit: habit,
-                      overlayKey: overlayKey,
-                    ),
-                  );
-                },
-              ),
-              PopupMenu(key: overlayKey),
-            ],
-          );
-        },
-      ),
+    return ListenableBuilder(
+      // Listen to changes in the loadHabits function
+      listenable: viewModel.loadHabits,
+      builder: (context, child) {
+        // If the function is still running, show a progress indicator.
+        if (viewModel.loadHabits.running || viewModel.habits == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
+    
+        // TODO: make an error widget
+        // If the function failed, press to try again.
+        if (viewModel.loadHabits.error) {
+          return const SizedBox();
+        }
+        // * If not error, or running, filter habits and build list.
+        final filteredHabits = viewModel.habits!
+            .where((habit) => !habit.isRetired)
+            .toList();
+    
+        return Stack(
+          children: [
+            CustomScrollView(
+              physics: const BouncingScrollPhysics(),
+              slivers: [
+                ...habitsSliverAppbar(context),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate((context, index) {
+                      final habit = filteredHabits[index];
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 12),
+                        child: HabitsGesturableCard(
+                          habit: habit,
+                          overlayKey: overlayKey,
+                        ),
+                      );
+                    }, childCount: filteredHabits.length),
+                  ),
+                ),
+              ],
+            ),
+            PopupMenu(key: overlayKey),
+          ],
+        );
+      },
     );
   }
 }
